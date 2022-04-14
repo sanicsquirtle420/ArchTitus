6871b20c5987e4eb320d2c17527133f7f1425d99
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2022-04-13 20:18:24 -0500

    Update 0-preinstall.sh

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -33,24 +33,6 @@ jrei  __' .::'
                     Automated Arch Linux Installer
 -------------------------------------------------------------------------
 
-Setting up mirrors for optimal download
-"
-source setup.conf
-iso=$(curl -4 ifconfig.co/country-iso)
-timedatectl set-ntp true
-pacman -S --noconfirm pacman-contrib terminus-font
-setfont ter-v22b
-sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
-pacman -S --noconfirm reflector rsync grub
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-echo -ne "
--------------------------------------------------------------------------
-                    Setting up $iso mirrors for faster downloads
--------------------------------------------------------------------------
-"
-reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
-mkdir /mnt &>/dev/null # Hiding error message if any
-echo -ne "
 -------------------------------------------------------------------------
                     Installing Prerequisites
 -------------------------------------------------------------------------
@@ -157,9 +139,7 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 pacstrap /mnt base linux linux-firmware sudo vim grub
-echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
 cp -R ${SCRIPT_DIR} /mnt/root/RacArch
-cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 mkdir /mnt/root
 cd /mnt/root
 git clone https://github.com/sanicsquirtle420/RacArch
