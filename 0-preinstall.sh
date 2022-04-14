0418a4b9a4733a14fb2c091919612355d0c25cc1
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2022-04-13 19:35:15 -0500

    Update 0-preinstall.sh

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -161,7 +161,7 @@ echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
 cp -R ${SCRIPT_DIR} /mnt/root/RacArch
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 mkdir /mnt/root
-cd /root
+cd /mnt/root
 git clone https://github.com/sanicsquirtle420/RacArch
 cd ~
 echo -ne "
