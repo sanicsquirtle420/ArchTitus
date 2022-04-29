25c98cd13cf3edd55a52ebc9e06936fdadebabb5
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2022-04-28 19:34:24 -0500

    Update 1-setup.sh

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -1,7 +1,23 @@
-# Installing Packages
+# chroot into /mnt
+arch-chroot /mnt
 
-cat /root/RacArch/pkg-files/pacman-pkgs.txt | while read line 
-do
-  echo "INSTALLING: ${line}"
-  sudo pacman -S --noconfirm --needed ${line}
-done
+# Installing Essential Packages
+pacman -S sudo networkmanager grub efibootmgr  git vim
+
+# Enableing / Configuring Essential Packages
+systemctl enable NetworkManager.service
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Finishing Steps
+echo "Congrats the basic Arch Linux system has been installed."
+sleep 1
+read -p "Type your username (All lowercase please)... " username
+useradd -m $username
+usermod -aG wheel ${username}
+echo "User ${username} has been created and has been given sudo privileges"
+sleep 1
+echo "Now let's set the password for '${username} ..."
+passwd $username
+
+echo "Perfect!"
+echo "Now reboot the system with 'reboot' then run '2-postinstall.sh'."
