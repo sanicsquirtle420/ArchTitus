09f32de6a1eadb466028781274a1fca22e043926
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2022-05-13 10:12:11 -0500

    Update 0-preinstall.sh

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -9,5 +9,5 @@ genfstab -U /mnt >> /mnt/etc/fstab
 # Prepares for next step
 sleep 1
 cp -r /root/RacArch /mnt/root/RacArch
-echo "Run 'arch-chroot /mnt' then run './1-setup.sh'."
+echo "Run 'arch-chroot /mnt' then cd into '~/RacArch/scripts` and run './1-setup.sh'."
 exit
